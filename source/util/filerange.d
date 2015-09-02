84daab94f631783dd9edd4c0d672ee1ef36a9612
Jesse Phillips <Jesse.K.Phillips@gmail.com>
2015-09-02 16:15:30 -0700

    Allow loading utf files

--- source/util/filerange.d
+++ source/util/filerange.d
@@ -2,20 +2,61 @@ module util.filerange;
 
 import std.mmfile;
 
-struct FileRange {
-private:
-	size_t index;
-	size_t end;
-	MmFile file;
-
-public:
-	static auto opCall(string file) {
-		FileRange fr;
+auto fileRange(ByteType = ubyte)(string file) {
+	import std.traits;
+	static if(is(ByteType == ubyte)) {
+		FileRange!ByteType fr;
 		fr.file = new MmFile(file);
 		fr.end = fr.file.length;
 		return fr;
+	} else
+	static if(isSomeChar!ByteType) {
+		UtfFileRange!ByteType u8fr;
+		u8fr.fr.file = new MmFile(file);
+		u8fr.fr.end = u8fr.fr.file.length;
+		u8fr.prime();
+		return u8fr;
+	} else
+		static assert(false, "Supported FileRange bytes: ubyte, dchar, wchar, char");
+}
+
+struct UtfFileRange(C) {
+	FileRange!C fr;
+	dchar cur;
+	@property auto empty() {
+		return cur == dchar.init;
 	}
 
+	auto popFront() {
+		assert(!empty, "Popping front of empty range");
+		if(fr.empty) {
+			cur = dchar.init;
+			return;
+		}
+		prime();
+	}
+
+	auto prime() {
+		import std.utf;
+		cur = fr.decodeFront;
+	}
+
+	auto front() {
+		return cur;
+	}
+
+	auto save() {
+		return this;
+	}
+}
+
+struct FileRange(ByteType = ubyte) {
+private:
+	ulong index;
+	ulong end;
+	MmFile file;
+
+public:
 	@property auto empty() {
 		assert(index <= end);
 		return index == end;
@@ -26,10 +67,10 @@ public:
 	}
 
 	auto front() {
-		return file[index];
+		return cast(ByteType)file[index];
 	}
 
-	@property auto length() {
+	@property ulong length() {
 		return end - index;
 	}
 
